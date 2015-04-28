21a5df42fa34da95f5b6d28cf11987c725688d99
Nabajit Bhadra <nxb148@case.edu>
2015-04-28 18:59:59 -0400

    Lab 5  Lab
    Course Creator Lab

---
+++ Lab 5/Lab 5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int classSize;
+            bool correct = false; 
+
+            while (!correct)
+            {
+            Course course = new Course();
+ 
+            Console.WriteLine("Please provide the name of the course?      ");
+            course.courseName = Console.ReadLine();
+            Console.WriteLine("What is the Course number?    ");
+            course.courseID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many Students in Course?     ");
+            classSize = Convert.ToInt32(Console.ReadLine());
+               
+            course.classStudents = new Student[classSize];
+           
+            for (int i = 0; i < classSize; i++)
+            {
+                
+                Console.WriteLine(String.Format("What is Student {0} name?   ",i+1));
+                string studentName = Console.ReadLine();
+                Console.WriteLine(String.Format("What is Student {0} ID?   ",i+1));
+                int studentID = Convert.ToInt32(Console.ReadLine());
+
+                course.classStudents[i] = new Student(studentName, studentID);
+                Student[] classStudents = new Student[i];
+                
+            }
+
+            Console.Write("Please recheck the information.Is it correct (yes/no)?     ");
+            correct = Console.ReadLine().StartsWith("y");
+
+
+            }
+
+        }
+    }
+}
---
+++ Lab 5/Lab 5/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5
+{
+    class Student
+    {
+        public string name;
+        public int ID;
+
+        public Student(string studentName, int studentID)
+        {
+            this.name = studentName;
+            this.ID = studentID;
+        }
+    }
+
+
+}
